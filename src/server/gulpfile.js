fcdd4cbed8ec993524578f951a79fde039ad1ddc
Mario B. Almeida <mariobrusarosco@gmail.com>
2017-10-25 11:41:26 -0200

    On production mode: Inside the APP PHP Class, the  variable gets changed

--- src/server/gulpfile.js
+++ src/server/gulpfile.js
@@ -1,26 +1,71 @@
-const gulp  = require('gulp');          // MAIN MODULE
-const del   = require('del');           // MODULE TO DELETE FILES AND FOLDERS
-const util  = require('gulp-util');     // UTILITIES: ['GET CLI PASSED VARIABLES']
+// =========================
+// MODULES
+const gulp          = require('gulp');                  // MAIN MODULE
+const _m            = require('gulp-load-plugins')();   // THIS VARIABLE WILL HOLD ALMOST !!! ALL PLUGINS
+const util          = require('gulp-util');             // UTILITIES: ['GET CLI PASSED VARIABLES']
+const replace       = require('gulp-replace');          // A MODULE TO CHANGE SOME LINE IN DEPLOYED FILES
+const filter        = require('gulp-filter');           // A MODULE FILTER A PIPE'S CONTENT...RESTORE THIS FILTERED CONTENT ETC...
+
+// =============== MODULES THAT FAILS TO BE LOADED INTO _m VARIABLE
+const del           = require('del');                   // MODULE TO DELETE FILES AND FOLDERS
 
 
-// gulp.task('clean',require('./tasks/clean')(del,'server-deploy'));
-gulp.task('clean',require('./tasks/clean')(del,'server-deploy/'));
-gulp.task('copy', require('./tasks/copy')(gulp));
+// =========================
+// BUILD CONFIGURATION
+const CONFIG = {
+    DEPLOY_PATH : './server-deployed',
+};
 
 
+// CLEAN PROCESS
+// =========================
+gulp.task('clean', () => {
+    return del.sync([
+        './'+ CONFIG.DEPLOY_PATH
+    ]);
+});
 
-console.log(util.env.production);
-
-
+// COPY PROCESS
+// =========================
+gulp.task('copy', () => {
+    const filterAppClass = _m.filter('**/App.php', {restore: true});
+    return gulp.src([
+                    './+(api|classes|configuration)/**/*',      
+                ])
+                /*
+                    WHEN RUNNING ON A PRODUCTION ENVIRONMENT... ON ALL FILES SELECTED ABOVE,
+                    FILTER THE MAIN APP CLASS NAMED 'App.php'
+                */
+                .pipe( (_m.util.env.production) ? filterAppClass : _m.util.noop() )
+                /*
+                    WHEN RUNNING ON A PRODUCTION ENVIRONMENT... CHANGE THE $ENV VARIABLE 
+                    VALUE FROM 'development' TO 'production'  
+                */
+                .pipe( (_m.util.env.production) 
+                        ? _m.replace(/\$ENV\s*=\s*(.*)\s*;/, (match,group1) => {
+                                return match.replace(group1,'"production"'); 
+                          })                
+                        : _m.util.noop()
+                ) 
+                /* 
+                    WHEN RUNNING ON A PRODUCTION ENVIRONMENT ....RESTORE THE INITIAL SET OF 
+                    FILES DEFINED ON gulp.src().. RIGHT AFTER THE 'return' KEYWORD 
+                */
+                .pipe( (_m.util.env.production) ? filterAppClass.restore : _m.util.noop() ) 
+                /* 
+                   WHEN RUNNING ON A PRODUCTION ENVIRONMENT... SET A DIFERENT DESTINATION PATH 
+                */
+                .pipe( (_m.util.env.production) ? gulp.dest( CONFIG.DEPLOY_PATH ) : _m.util.noop() ) 
+});
 
 
 gulp.task('default', 
-    [
-        'clean',
-        'copy'
-    ],
-    
-    () => {
-    
-    }
+                    [
+                        'clean',
+                        'copy'
+                    ],
+                    
+                    () => {
+                    
+                    }
 );
